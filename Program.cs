2e8b9b1d364c11901125abe73df1d4c0478a486a
Andrew Santos Maia <andrew.maia@alperseguros.com.br>
2024-03-30 22:15:10 -0300

    Exemplo de cancelamento de operação

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Cancellation Token é criado com tempo de cancelamento de 10 segundos
+//Se o método Cancel não for chamado manualmente, a operação é cancelada automaticamente após 10 segundos
+CancellationTokenSource cancellationTokenSource = new(10000);
+var token = cancellationTokenSource.Token;
+ExecutarOperacaoAssincrona(token); // A operação assincrona é iniciada de forma assincrona
+Console.ReadLine(); // Aguarda o usuário pressionar enter para cancelar a operação 
+if(!token.IsCancellationRequested) // Se a operação já não foi cancelada por tempo, prossegue com o cancelamento manual
+{
+    cancellationTokenSource.Cancel();
+    Console.ReadLine();
+}
+
+async Task ExecutarOperacaoAssincrona(CancellationToken cancellationToken)
+{
+    for(int i=0; i<100; i++)
+    {
+        Console.WriteLine(i);
+        await Task.Delay(1000);
+        if(cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine("Operação cancelada");
+            return;
+        }
+    }
+}
+
