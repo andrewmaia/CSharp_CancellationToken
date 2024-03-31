9477abe7b808ef2f078e59abb80b69fc4927a657
Andrew Santos Maia <andrew.maia@alperseguros.com.br>
2024-03-31 09:38:16 -0300

    Novos testes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿//Cancellation Token é criado com tempo de cancelamento de 10 segundos
+﻿//////****Versão utilizando IsCancellationRequested****//////
+
+//Cancellation Token é criado com tempo de cancelamento de 10 segundos
 //Se o método Cancel não for chamado manualmente, a operação é cancelada automaticamente após 10 segundos
-CancellationTokenSource cancellationTokenSource = new(10000);
+/*CancellationTokenSource cancellationTokenSource = new(10000);
 var token = cancellationTokenSource.Token;
-ExecutarOperacaoAssincrona(token); // A operação assincrona é iniciada de forma assincrona
+var task = ExecutarOperacaoAssincrona(token); // A operação assincrona é iniciada de forma assincrona
 Console.ReadLine(); // Aguarda o usuário pressionar enter para cancelar a operação 
 if(!token.IsCancellationRequested) // Se a operação já não foi cancelada por tempo, prossegue com o cancelamento manual
 {
@@ -22,5 +24,32 @@ async Task ExecutarOperacaoAssincrona(CancellationToken cancellationToken)
             return;
         }
     }
+}*/
+
+
+//////****Versão utilizando ThrowIfCancellationRequested****//////
+
+CancellationTokenSource cancellationTokenSource = new(10000);
+var token = cancellationTokenSource.Token;
+var task = ExecutarOperacaoAssincrona(token); // A operação assincrona é iniciada de forma assincrona
+Console.ReadLine(); // Aguarda o usuário pressionar enter para cancelar a operação 
+if(!token.IsCancellationRequested) // Se a operação já não foi cancelada por tempo, prossegue com o cancelamento manual
+    cancellationTokenSource.Cancel();
+
+try{
+    await task;
+}
+catch(OperationCanceledException)
+{
+    Console.WriteLine("Operação cancelada");
 }
 
+async Task ExecutarOperacaoAssincrona(CancellationToken cancellationToken)
+{
+    for(int i=0; i<100; i++)
+    {
+        Console.WriteLine(i);
+        await Task.Delay(1000);
+        cancellationToken.ThrowIfCancellationRequested();
+    }
+}
